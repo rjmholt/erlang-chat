ca628d7ead4612916897566cfffe13b5382863ab
Robert Holt <rjmholt@gmail.com>
2016-06-04 11:48:03 +1000

    Added a room server and a guest name generation function

--- src/erlang_chat_room.erl
+++ src/erlang_chat_room.erl
@@ -4,15 +4,14 @@
 %%% @doc
 %%%
 %%% @end
-%%% Created : 2016-06-03 18:55:42.370790
+%%% Created : 2016-06-03 22:13:26.265562
 %%%-------------------------------------------------------------------
--module(erlang_chat_nameservice).
+-module(erlang_chat_room).
 
 -behaviour(gen_server).
 
 %% API
--export([start_link/0,
-         new_user/0]).
+-export([new/2]).
 
 %% gen_server callbacks
 -export([init/1,
@@ -23,16 +22,8 @@
          code_change/3]).
 
 -define(SERVER, ?MODULE).
--define(MAINHALL, <<"MainHall">>).
 
-% The globally required details of a room
--record(room, {name, pid, users=#{}}).
-% The globally required details of a user
--record(user, {pid, name}).
-% The nameserver's state
--record(state, {users=ets:new(users, [{keypos, #user.name}]),
-                rooms=ets:new(rooms, [{keypos, #room.name}])}).
-
+-record(state, {name, users = #{}, owner = <<>>}).
 
 %%%===================================================================
 %%% API
@@ -40,23 +31,13 @@
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Starts the server
+%% Creates a new room service
 %%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @spec new() -> {ok, Pid} | ignore | {error, Error}
 %% @end
 %%--------------------------------------------------------------------
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Add a new user to the server and give them a name automatically
-%%
-%% @spec new_user() -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
-new_user() ->
-    gen_server:call(?SERVER, new_user).
+new(ServerName, Owner) ->
+    gen_server:start_link(?MODULE, [ServerName, Owner], []).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -73,12 +54,8 @@ new_user() ->
 %%                     {stop, Reason}
 %% @end
 %%--------------------------------------------------------------------
-init([]) ->
-    State    = #state{},
-    {ok, MainHall} = erlang_chat_room:new(?MAINHALL),
-    MH_Entry = #room{name=?MAINHALL, pid=MainHall},
-    ets:insert(State#state.rooms, MH_Entry),
-    {ok, State}.
+init([ServerName, Owner]) ->
+    {ok, #state{name=ServerName, owner=Owner}}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -94,12 +71,6 @@ init([]) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
-handle_call(new_user, UPid, State) ->
-    Name = generate_new_username(State#state.users),
-    User = #user{pid = UPid, name = Name},
-    NewState = add_user(State, User),
-    {reply, {ok, Name}, NewState};
-
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
@@ -159,64 +130,6 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Generate a new guest username, based on the usernames already in use
-%%
-%% @spec generate_new_username(UserTable) -> binary()
-%% @end
-%%--------------------------------------------------------------------
-generate_new_username(UserTable) ->
-    Num = get_lowest_unused_guest_num(UserTable),
-    NumBin = list_to_binary(integer_to_list(Num)),
-    <<"guest", NumBin>>.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Find the lowest integer not used in a username of the form "guest<num>"
-%%
-%% @spec get_lowest_unused_guest_num(UserTable) -> integer()
-%% @end
-%%--------------------------------------------------------------------
-get_lowest_unused_guest_num(UserTable) ->
-    GuestNums = ets:foldl(fun collect_guest_num/2, [], UserTable),
-    lowest_not_in_list(GuestNums).
 
-collect_guest_num(UserEntry, NumList) ->
-    Name = UserEntry#user.name,
-    case Name of
-        <<"guest", NumBin/binary>> ->
-            Num = list_to_integer(binary_to_list(NumBin)),
-            [Num | NumList];
-        _ ->
-            NumList
-    end.
 
-lowest_not_in_list(List) ->
-    SeenArray = array:new(length(List), {default, unseen}),
-    lowest_acc(List, SeenArray).
-
-lowest_acc([Num|Ns], SeenArray) ->
-    SeenArray1 = case Num-1 < array:size(SeenArray) of
-                     false ->
-                         SeenArray;
-                     true ->
-                        array:set(Num-1, seen, SeenArray)
-                 end,
-    lowest_acc(Ns, SeenArray1);
-
-lowest_acc([], SeenArray) ->
-    lowest_zero_index(0, SeenArray).
-
-lowest_zero_index(Idx, SeenArray) ->
-    case Idx >= array:size(SeenArray) of
-        true  -> Idx+1;
-        false ->
-            case array:get(Idx, SeenArray) of
-                unseen -> Idx+1;
-                seen   ->
-                    lowest_zero_index(Idx+1, SeenArray)
-            end
-    end.
