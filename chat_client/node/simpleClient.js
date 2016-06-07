6f53fc61ecfaefea591cfe1333ef92c33f4a1df2
Robert Holt <rjmholt@gmail.com>
2016-06-07 11:50:45 +1000

    Changed the node client to not need to constantly check for handshake conditions

--- chat_client/node/simpleClient.js
+++ chat_client/node/simpleClient.js
@@ -1,13 +1,39 @@
 var net      = require('net');
 
-var clientState = { name: '',
-                    room: '',
-                    prompt: function () {
-                        if (this.name !== '' && this.room !== '') {
-                            process.stdout.write('[' + this.room + '] ' +
-                                this.name + '> ');
-                        }
-                    }
+// Incoming message types
+const S_MESSAGE    = 'message';
+const NEWIDENTITY  = 'newidentity';
+const ROOMCHANGE   = 'roomchange';
+const ROOMLIST     = 'roomlist';
+const ROOMCONTENTS = 'roomcontents';
+
+// Outgoing message types
+const C_MESSAGE      = 'message';
+const JOIN           = 'join';
+const QUIT           = 'quit';
+const IDENTITYCHANGE = 'identitychange';
+const WHO            = 'who';
+const LIST           = 'list';
+const CREATEROOM     = 'createroom';
+const DELETE         = 'delete';
+const KICK           = 'kick';
+
+// Client states
+const AWAITING_NAME = 1;
+const AWAITING_ROOM = 2;
+const READY         = 3;
+
+var clientState = { readiness:  AWAITING_NAME,
+                    name:       '',
+                    room:       '',
+                    prompt: 
+                        function () {
+                            if (this.name !== '' && this.room !== '') {
+                                process.stdout.write('[' + this.room + '] ' +
+                                    this.name + '> ');
+                            }
+                        },
+                    receiveMessage: (m) => initialHandshake(m, this.readiness),
                   };
 
 var client = new net.Socket();
@@ -27,12 +53,59 @@ function receiveData(data) {
 
     for (var objStr of objStrings) {
         var msg = JSON.parse(objStr);
-        processMessage(msg);
-        clientState.prompt();
+        console.log('Message received:', msg);
+        clientState.receiveMessage(msg);
+        clientState.receiveMessage = getNextFunction(clientState.readiness);
     }
 }
 
+function getNextFunction(state) {
+    switch (state) {
+    case READY:
+        return (m) => processMessage(m);
+
+    case AWAITING_NAME:
+        return (m) => initialHandshake(m, AWAITING_NAME);
+
+    case AWAITING_ROOM:
+        return (m) => initialHandshake(m, AWAITING_ROOM);
+    }
+
+    // If nothing is going right, just help the user to get themselves set up
+    return (m) => initialHandshake(m, READY);
+}
+
+function initialHandshake(msg, state) {
+    if (typeof msg !== 'object') {
+        console.log('Did not receive and object:', msg);
+        return;
+    }
+
+    switch (msg.type) {
+    case NEWIDENTITY:
+        if (msg.former === clientState.name && msg.former === '') {
+            clientState.name = msg.identity;
+            clientState.readiness = AWAITING_ROOM;
+        }
+        break;
+
+    case ROOMCHANGE:
+        if (msg.former === clientState.room && msg.former === '') {
+            clientState.room = msg.roomid;
+            clientState.readiness = READY;
+            clientState.prompt();
+        }
+        break;
+    }
+    return;
+}
+
 function processMessage(msg) {
+    if (typeof msg !== 'object') {
+        console.log('Message is not an object:', msg);
+        return;
+    }
+
     console.log('Message Received:', msg);
 
     switch (msg.type) {
@@ -108,71 +181,80 @@ function processMessage(msg) {
         default:
             console.log('Unknown message:', msg);
     }
+
+    clientState.prompt();
 }
 
 function processUserInput(input) {
     // No hash means input entered was a message
     if (input[0] !== '#') {
-        return { type: 'message', message: input.slice(0, input.length-1) };
+        return { type: C_MESSAGE, message: input.slice(0, input.length-1) };
     }
     // Otherwise it's a command
     else {
         var tokens = input.slice(1).split(/\s+/);
 
         switch (tokens[0]) {
-        case 'join':
+        case JOIN:
+            if (tokens.length !== 2) {
+                clientState.prompt();
+                return;
+            }
+            return { type: JOIN, roomid: tokens[1] };
+
+        case IDENTITYCHANGE:
+            if (tokens.length !== 2) {
+                clientState.prompt();
+                return;
+            }
+            return { type: IDENTITYCHANGE, identity: tokens[1] };
+
+        case QUIT:
+            return { type: QUIT };
+
+        case WHO:
+            if (tokens.length !== 2) {
+                clientState.prompt();
+                return;
+            }
+            return { type: WHO, roomid: tokens[1] };
+
+        case LIST:
+            return { type: LIST };
+
+        case CREATEROOM:
+            if (tokens.length !== 2) {
+                clientState.prompt();
+                return;
+            }
+            return { type: CREATEROOM, roomid: tokens[1] };
+
+        case DELETE:
             if (tokens.length !== 2) {
                 return;
             }
-            return { type: tokens[0], roomid: tokens[1] };
+            return { type: DELETE, roomid: tokens[1] };
 
-        case 'identitychange':
-            if (tokens.length !== 2) {
-                return;
-            }
-            return { type: tokens[0], identity: tokens[1] };
-
-        case 'quit':
-            return { type: tokens[0] };
-
-        case 'who':
-            if (tokens.length !== 2) {
-                return;
-            }
-            return { type: tokens[0], roomid: tokens[1] };
-
-        case 'list':
-            return { type: tokens[0] };
-
-        case 'createroom':
-            if (tokens.length !== 2) {
-                return;
-            }
-            return { type: tokens[0], roomid: tokens[1] };
-
-        case 'delete':
-            if (tokens.length !== 2) {
-                return;
-            }
-            return { type: tokens[0], roomid: tokens[1] };
-
-        case 'kick':
+        case KICK:
             if (tokens.length !== 4) {
+                clientState.prompt();
                 return;
             }
-            return { type: tokens[0], roomid: tokens[1],
+            return { type: KICK, roomid: tokens[1],
                      time: parseInt(tokens[2], 10), identity: tokens[3]};
 
         // First part doesn't recognise a supported command,
         // send it as an ordinary message instead
         default:
-            return { type: 'message', message: input };
+            return { type: C_MESSAGE, message: input };
         }
     }
 }
 
 function doInputMessage(inBuf) {
     var message = processUserInput(inBuf.toString('utf8'));
-    console.log(message);
-    client.write(JSON.stringify(message), 'utf8');
+    if (typeof message === 'object') {
+        console.log(message);
+        client.write(JSON.stringify(message), 'utf8');
+    }
 }
